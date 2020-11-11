12adeef286a1fc648663afd335cc238afa428dc3
Joshua Retallick <josh@old-empire.co.uk>
2020-11-11 15:45:39 +0000

    Created web_helper

--- spec/features/view_hit_points_spec.rb
+++ spec/features/view_hit_points_spec.rb
@@ -8,16 +8,3 @@ feature 'View hit points' do
     expect(page).to have_content 'Health'
   end
 end
-
-
-
-
-# feature 'Enter names' do
-#   scenario 'submitting names' do
-#     visit('/')
-#     fill_in :player_1_name, with: 'Dave'
-#     fill_in :player_2_name, with: 'Mittens'
-#     click_button 'Submit'
-#     expect(page).to have_content 'Dave vs. Mittens'
-#   end
-# end
