7e05d3007fbca0f5270c345b09868e2852a0a5c5
Joshua Retallick <josh@old-empire.co.uk>
2020-11-13 16:19:34 +0000

    Added styling

--- app.rb
+++ app.rb
@@ -1,21 +1,16 @@
 require 'sinatra/base'
-require_relative './lib/player.rb'
-require_relative './lib/game.rb'
+require './lib/player.rb'
+require './lib/game.rb'
+require './lib/attack'
 
 class Battle < Sinatra::Base
-
-  configure do
     enable :sessions
-    set :session_secret, "secret"
-  end
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-#    $player_1 = Player.new(params[:player_1_name])
-#    $player_2 = Player.new(params[:player_2_name])
     player_1 = Player.new(params[:player_1_name])
     player_2 = Player.new(params[:player_2_name])
     $game = Game.new(player_1, player_2)
@@ -23,23 +18,32 @@ class Battle < Sinatra::Base
   end
 
   get '/play' do
-#    @player_1_name = $player_1.name
-#    @player_2_name = $player_2.name
     @game = $game
     erb :play
   end
 
+  post '/attack' do
+    Attack.run($game.opponent_of($game.current_turn))
+    if $game.game_over?
+      redirect '/game-over'
+    else
+      redirect '/attack'
+    end
+  end
+
   get '/attack' do
-#    @player_1 = $player_1
-#    @player_2 = $player_2
     @game = $game
-    @game.attack(@game.opponent_of(@game.current_turn))
     erb :attack
   end
 
   post '/switch-turns' do
     $game.switch_turns
     redirect('/play')
+  end
+
+  get '/game-over' do
+    @game = $game
+    erb :game_over
   end
 
   run! if app_file == $0
--- spec/features/attack_player2_spec.rb
+++ spec/features/attack_player2_spec.rb
@@ -5,29 +5,37 @@ feature 'Attack' do
     expect(page).to have_content 'Dave attacked Mittens'
   end
 
-  scenario 'be attached by Player 2' do
+  scenario 'be attacked by Player 2' do
     sign_in_and_play
-    click_button 'Attack'
-    click_button 'OK'
+    attack_and_confirm
     click_button 'Attack'
     expect(page).to have_content 'Mittens attacked Dave'
   end
 
   scenario 'Attack reduces player 2 health by 10' do
     sign_in_and_play
-    click_button 'Attack'
-    click_button 'OK'
+    attack_and_confirm
     expect(page).not_to have_content 'Mittens: 60HP'
     expect(page).to have_content 'Mittens: 50HP'
   end
 
   scenario 'reduce Player 1 HP by 10' do
     sign_in_and_play
-    click_button 'Attack'
-    click_button 'OK'
-    click_button 'Attack'
-    click_button 'OK'
+    2.times { attack_and_confirm }
     expect(page).not_to have_content 'Dave: 60HP'
     expect(page).to have_content 'Dave: 50HP'
   end
+
+  scenario 'reduce Player 2 HP by a random amount' do
+    sign_in_and_play
+    attack_and_confirm
+    expect(page).not_to have_content 'Mittens: 60HP'
+  end
+
+  scenario 'reduce Player 1 HP by a random amount' do
+    sign_in_and_play
+    2.times { attack_and_confirm }
+    expect(page).not_to have_content 'Dave: 60HP'
+  end
+
 end
